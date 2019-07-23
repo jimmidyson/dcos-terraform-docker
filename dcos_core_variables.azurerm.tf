717fa36a61d3ccc9e1ac8d3662f8fba79db8771f
Jimmi Dyson <jimmidyson@gmail.com>
2019-07-23 11:35:21 +0100

    chore: Upgrade to DC/OS 1.13.2 by default

--- dcos_core_variables.azurerm.tf
+++ dcos_core_variables.azurerm.tf
@@ -14,7 +14,7 @@ variable "dcos_install_mode" {
 }
 
 variable "dcos_version" {
-  default     = "1.12.3"
+  default     = "1.13.2"
   description = "specifies which dcos version instruction to use. Options: `1.9.0`, `1.8.8`, etc. _See [dcos_download_path](https://github.com/dcos/tf_dcos_core/blob/master/download-variables.tf) or [dcos_version](https://github.com/dcos/tf_dcos_core/tree/master/dcos-versions) tree for a full list._"
 }
 
