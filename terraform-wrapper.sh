50c8adfa96cb14cd304f99a181a5a6024c567634
Jimmi Dyson <jimmidyson@gmail.com>
2020-05-14 16:23:48 +0100

    chore: Satisfy shfmt
    Latest pre-commit check failed.

--- terraform-wrapper.sh
+++ terraform-wrapper.sh
@@ -4,8 +4,8 @@ set -xeuo pipefail
 IFS=$'\n\t'
 
 if [ -n "${SSH_PRIVATE_KEY_FILE:-}" ]; then
-  eval $(ssh-agent | head -2)
-  ssh-add "${SSH_PRIVATE_KEY_FILE}"
+	eval $(ssh-agent | head -2)
+	ssh-add "${SSH_PRIVATE_KEY_FILE}"
 fi
 
 terraform init >&2
